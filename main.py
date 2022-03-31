d922d1cd0b647c020b1f341b6f7bedcacab19108
Aaron Djangmah <96618832+zezecodes@users.noreply.github.com>
2022-03-31 18:39:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,3 @@ def recursive_binary_search(list, target):
 def verify(result):
     print("Target found: ", result)
 
-numbers = [1,2,3,4,5,6,7,8,9,10]
-result = recursive_binary_search(numbers, 9)
-verify(result)
-  
